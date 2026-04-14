dc0cb985916ac216714ff751c942c96108b14064
nshuti <nshutikope@gmail.com>
2026-04-14 16:07:10 +0200

    Add and update page components (Help pages, NotFound)

---
+++ src/pages/Help/faq.jsx
@@ -0,0 +1,9 @@
+export function Faq() {
+    return (<div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta odit tempore ex? Ad quis rerum, reprehenderit voluptatum nam libero quia! Distinctio accusamus esse officiis inventore explicabo possimus cumque ab veritatis?</p>
+        <br />
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, molestias dolorem reiciendis voluptatum ratione harum temporibus ullam optio deserunt officiis fugiat, animi quia est tenetur natus ab cumque esse sunt!</p>
+        <br />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto harum hic officiis repellendus et, sapiente, mollitia, esse ipsam quaerat rem aliquam animi! Ad aperiam nihil quo eaque accusamus, at odio?</p>
+    </div> )
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export function NotFound() {
+    return (<div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis quas nulla doloremque repellat a provident ipsa! Impedit eius quae inventore corrupti dicta, sapiente quod recusandae maiores cumque minima hic incidunt.i</p>
+    <p>Got to <Link to='/'>Homepage</Link></p>
+    </div>
+    )
+}
