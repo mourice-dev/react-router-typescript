fd4f4de8dfe0bb04c679b20e686975cc6809ff02
nshuti <nshutikope@gmail.com>
2026-04-14 16:07:18 +0200

    Update App.js with new routes, imports, and router setup

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { RootLayout } from "./layout/RootLayout.js";
+
+import { RootLayout } from "./layout/RootLayout.jsx";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
 import { Contact } from "./pages/Contact.jsx";
+import { HelpLayout } from "./layout/HelpLayout.jsx";
+import { Service } from "./pages/Help/service.jsx";
+import { Faq } from "./pages/Help/faq.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
 
 function App() {
   const Router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='help' element={<HelpLayout />}>
+          <Route path='service' element={<Service />} />
+          <Route path='faq' element={<Faq />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
       </Route>,
     ),
   );
