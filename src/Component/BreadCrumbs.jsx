696daf06699900220b7643ac676c0b8f2ffccc1e
nshuti <nshutikope@gmail.com>
2026-04-16 10:19:40 +0200

    Change breadcrumb separator to >

--- src/Component/BreadCrumbs.jsx
+++ src/Component/BreadCrumbs.jsx
@@ -7,13 +7,15 @@ export function BreadCrumbs() {
     
     const crumbs = location.pathname.split('/')
     .filter((crumb) => crumb !=='')
-    .map((crumb) => { currentLink+=`/${crumb}`
+    .map((crumb, index, arr) => { 
+      currentLink+=`/${crumb}`
+      const isLast = index === arr.length - 1;
       return(
         <span className="crumb" key={crumb}>
           <Link to={currentLink} className={isLast ? 'active-crumb' : ''}>
             {crumb.charAt(0).toUpperCase() + crumb.slice(1)}
           </Link>
-          {!isLast && <span className="crumb-separator">›</span>}
+          {!isLast && <span className="crumb-separator">&gt;</span>}
         </span>
       )
     });
